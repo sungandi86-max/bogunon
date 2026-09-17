e5bca02087e08ba2c399f0219b782bbbf120138b
sungandi86-max <sungandi86@gmail.com>
2026-09-17 10:07:38 +0900

    chore: align practical tools index migration with remote version

---
+++ supabase/migrations/20260917010651_add_practical_tools_fk_indexes.sql
@@ -0,0 +1,5 @@
+create index practical_tools_owner_id_idx
+  on public.practical_tools(owner_id);
+
+create index practical_schedule_tools_tool_id_idx
+  on public.practical_schedule_tools(tool_id);
