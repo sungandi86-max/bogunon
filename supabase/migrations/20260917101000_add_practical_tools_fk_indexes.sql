68b9d48055653c9adb7811b8abdba9057dd4941b
sungandi86-max <sungandi86@gmail.com>
2026-09-17 10:07:54 +0900

    chore: remove superseded index migration timestamp

--- supabase/migrations/20260917101000_add_practical_tools_fk_indexes.sql
+++
@@ -1,5 +0,0 @@
-create index practical_tools_owner_id_idx
-  on public.practical_tools(owner_id);
-
-create index practical_schedule_tools_tool_id_idx
-  on public.practical_schedule_tools(tool_id);
